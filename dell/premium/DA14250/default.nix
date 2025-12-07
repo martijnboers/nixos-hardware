7d6c25ea467d52163ee7ea5c23b591eda05461c4
Martijn Boers <martijn@boers.email>
2025-12-07 16:36:50 +0100

    Don't patch

--- dell/premium/DA14250/default.nix
+++ dell/premium/DA14250/default.nix
@@ -14,16 +14,4 @@
 
   # This will save you money and possibly your life!
   services.thermald.enable = lib.mkDefault true;
-
-  boot.kernelPatches = [
-    {
-      name = "enable-soundwire-drivers";
-      patch = null;
-      extraConfig = ''
-        SND_SOC_INTEL_USER_FRIENDLY_LONG_NAMES y
-        SND_SOC_INTEL_SOUNDWIRE_SOF_MACH m
-        SND_SOC_RT1308 m
-      '';
-    }
-  ];
 }
