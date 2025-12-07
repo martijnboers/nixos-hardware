ce52e2e025a3d817f0d5fc24557a05c792d9540d
Martijn Boers <martijn@boers.email>
2025-12-07 15:12:26 +0100

    From xps

--- dell/premium/DA14250/default.nix
+++ dell/premium/DA14250/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ../../../common/cpu/intel
@@ -8,4 +8,22 @@
 
   # Includes the Wi-Fi and Bluetooth firmware
   hardware.enableRedistributableFirmware = true;
+
+  boot.kernelParams = lib.mkDefault [ "acpi_rev_override" ];
+  boot.kernelModules = lib.mkDefault [ "kvm-intel" ];
+
+  # This will save you money and possibly your life!
+  services.thermald.enable = lib.mkDefault true;
+
+  boot.kernelPatches = [
+    {
+      name = "enable-soundwire-drivers";
+      patch = null;
+      extraConfig = ''
+        SND_SOC_INTEL_USER_FRIENDLY_LONG_NAMES y
+        SND_SOC_INTEL_SOUNDWIRE_SOF_MACH m
+        SND_SOC_RT1308 m
+      '';
+    }
+  ];
 }
